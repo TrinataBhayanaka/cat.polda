5bcc2e58ae9977daeeea37a57670f6d636c05c0e
andreassbayu <andreass.bayu@gmail.com>
2016-01-27 20:17:01 +0700

    time

--- app/controller/login.php
+++ app/controller/login.php
@@ -31,21 +31,19 @@ class login extends Controller {
         // db($_COOKIE);
         if(isset($_COOKIE['id_peserta']))
         {
-            $status = $this->models->getData('generated_soal',0,"id_peserta = {$_COOKIE['id_peserta']} AND id_kategori = {$_COOKIE['id_kategori']}");
-            if($status){
-                if($status['status'] == 1 || $status['status'] == 2 || $status['status'] == 3)
-                {
-                     $getUser = $this->models->getData('master_peserta',0,"id_peserta = {$_COOKIE['id_peserta']}");
-                     $validateData = $this->loginHelper->local($getUser);
+            $status = $this->models->getData('generated_soal',0,"id_peserta = {$_COOKIE['id_peserta']} AND id_ujian = {$_COOKIE['id_ujian']}");
+            if($status['status'] == 1 || $status['status'] == 2 || $status['status'] == 3)
+            {
+                 $getUser = $this->models->getData('master_peserta',0,"id_peserta = {$_COOKIE['id_peserta']}");
+                 $validateData = $this->loginHelper->local($getUser);
 
-                     if ($validateData){
-                        redirect($basedomain.$CONFIG['default']['default_view']);
-                    }else{
-                        redirect($basedomain.$CONFIG['default']['login']);
-                    }
-                    exit;
-                } 
-            }
+                 if ($validateData){
+                    redirect($basedomain.$CONFIG['default']['default_view']);
+                }else{
+                    redirect($basedomain.$CONFIG['default']['login']);
+                }
+                exit;
+            } 
 
         }
         $materi = $this->models->getData('ujian',1,'status = 1');
